596a65ff4abb62d9e315c945e9b2edcd250e533f
mazenolama <44679914+mazenolama@users.noreply.github.com>
2021-07-08 13:55:11 +0200

    make it betteradaf

--- config/Database.php
+++ config/Database.php
@@ -1,16 +1,11 @@
 <?php 
-    define('DB_HOST','remotemysql.com');
-    define('DB_NAME','F0SaNkPCly');
-    define('DB_USERNAME','F0SaNkPCly');
-    define('DB_PASSWORD','ky3HtIoH0Y@');
   class Database {
     // DB Params
-    /*private $host = 'remotemysql.com';
+    private $host = 'remotemysql.com';
     private $db_name = 'F0SaNkPCly';
     private $username = 'F0SaNkPCly';
-    private $db_pass = 'ky3HtIoH0Y@';
-    private $conn;*/
-
+    private $db_pass = 'FJ2YKUB7gW';
+    private $conn;
     
 
     // DB Connect
@@ -19,7 +14,7 @@
 
       try 
       { 
-        $this->conn = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME,DB_USERNAME, DB_PASSWORD);
+        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->db_pass);
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       } 
       
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+
+    define('DB_HOST','remotemysql.com');
+    define('DB_NAME','F0SaNkPCly');
+    define('DB_USERNAME','F0SaNkPCly');
+    define('DB_PASSWORD','ky3HtIoH0Y@');
+
+    
